f8643d847624fb825a85224542c88b71338f5c4a
Hannes Kasel <hannes.kasel@advokat.at>
2023-10-10 11:38:44 +0200

    zwischenspeicherung

--- Reservoom/ViewModels/ReservationListingViewModel.cs
+++ Reservoom/ViewModels/ReservationListingViewModel.cs
@@ -2,10 +2,8 @@
 using Reservoom.Models;
 using Reservoom.Services;
 using Reservoom.Stores;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Windows.Input;
 
 namespace Reservoom.ViewModels
